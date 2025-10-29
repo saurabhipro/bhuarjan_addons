4de5740b122b1ae1ab46955888cc98165a0d8c1e
anjli <anjlis062024@gmail.com>
2025-10-29 14:14:56 +0530

    Hide side Menu Naming odoo

--- debrand_odoo/__manifest__.py
+++ debrand_odoo/__manifest__.py
@@ -13,30 +13,30 @@
                              'portal',
                             ],
   "data"                 :  [
-                             'views/res_config_view.xml',
-                             'views/web_client_template.xml',
-                             'views/portal_templates.xml',
-                             'views/email_templates.xml',
-                             'views/res_users.xml',
-                             'data/data.xml',
+                            #  'views/res_config_view.xml',
+                            #  'views/web_client_template.xml',
+                            #  'views/portal_templates.xml',
+                            #  'views/email_templates.xml',
+                            #  'views/res_users.xml',
+                            #  'data/data.xml',
                             ],
   "qweb"                 :  [
-                             'static/src/xml/base.xml',
-                             'static/src/xml/client_action.xml',
+                            #  'static/src/xml/base.xml',
+                            #  'static/src/xml/client_action.xml',
                             ],
   "assets"               : {  
                             "web.assets_backend": [
                                 # '/debrand_odoo/static/src/js/web_client.js',
-                                '/debrand_odoo/static/src/js/dialog.js',
-                                '/debrand_odoo/static/src/js/my_widget.js',
+                                # '/debrand_odoo/static/src/js/dialog.js',
+                                # '/debrand_odoo/static/src/js/my_widget.js',
                                 '/debrand_odoo/static/src/js/user_menu.js',
-                                '/debrand_odoo/static/src/js/mail_dialog.js'
+                                # '/debrand_odoo/static/src/js/mail_dialog.js'
                             ],
-                            'web.assets_qweb': [
-                                'debrand_odoo/static/src/xml/dashboard.xml',
-                                'debrand_odoo/static/src/xml/base.xml',
-                                'debrand_odoo/static/src/xml/client_action.xml',
-                            ],
+                            # 'web.assets_qweb': [
+                            #     'debrand_odoo/static/src/xml/dashboard.xml',
+                            #     'debrand_odoo/static/src/xml/base.xml',
+                            #     'debrand_odoo/static/src/xml/client_action.xml',
+                            # ],
                          },
   "images"               :  [''],
   "application"          :  True,

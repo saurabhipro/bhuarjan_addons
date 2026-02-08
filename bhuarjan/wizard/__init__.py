51da137a200c34a7e633bafebd8c4259c799c8b9
saurabhipro <saurabh.mehrotrag@gmail.com>
2026-02-08 21:20:46 +0530

    fix: Merge widget_owl and image_capture to resolve Owl console errors

--- bhuarjan/wizard/__init__.py
+++ bhuarjan/wizard/__init__.py
@@ -7,3 +7,4 @@ from . import section8_approve_reject_wizard
 from . import reject_survey_wizard
 from . import reject_railways_survey_wizard
 from . import gantt_report_wizard
+from . import image_capture
